f481c95bbb70c70a9ff2c0f67b9a57736e65f8f1
HowellYang <th15817161961@gmail.com>
2020-05-29 20:42:04 +0800

    fix : redis

--- atomscat-provider-service/src/main/java/com/atomscat/provider/serviceImpl/CustomerServiceImpl.java
+++ atomscat-provider-service/src/main/java/com/atomscat/provider/serviceImpl/CustomerServiceImpl.java
@@ -4,10 +4,12 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.atomscat.provider.request.CustomerInfoRequest;
 import com.atomscat.provider.response.CustomerInfoResponse;
 import com.atomscat.provider.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
@@ -31,6 +33,7 @@ public class CustomerServiceImpl implements CustomerService {
         redisTemplate.opsForValue().set("key_obj", customerInfoRequest);
         CustomerInfoResponse customerInfoResponse = new CustomerInfoResponse();
         customerInfoResponse.setName(stringRedisTemplate.opsForValue().get("key_string"));
+        log.info(customerInfoRequest.getName());
         return customerInfoResponse;
     }
 
