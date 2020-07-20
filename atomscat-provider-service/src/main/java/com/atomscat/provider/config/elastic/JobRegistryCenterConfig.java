c123f330d774c3c785b7a4246323724d29ad36fa
HowellYang <th15817161961@gmail.com>
2020-07-20 19:58:49 +0800

    fix : nacos

--- atomscat-provider-service/src/main/java/com/atomscat/provider/config/elastic/JobRegistryCenterConfig.java
+++ atomscat-provider-service/src/main/java/com/atomscat/provider/config/elastic/JobRegistryCenterConfig.java
@@ -1,8 +1,8 @@
 package com.atomscat.provider.config.elastic;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnExpression("'${elastic-job.servers}'.length() > 0")
 public class JobRegistryCenterConfig {
 
+    @NacosValue(value = "${elastic-job.servers}", autoRefreshed = true)
+    private String servers;
+
+    @NacosValue(value = "${elastic-job.namespace}", autoRefreshed = true)
+    private String namespace;
+
     @Bean(initMethod = "init")
-    public ZookeeperRegistryCenter regCenter(@Value("${elastic-job.servers}") final String servers,
-                                             @Value("${elastic-job.namespace}") final String namespace) {
+    public ZookeeperRegistryCenter regCenter() {
         return new ZookeeperRegistryCenter(new ZookeeperConfiguration(servers, namespace));
     }
 
